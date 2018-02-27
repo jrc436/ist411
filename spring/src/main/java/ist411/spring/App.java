38e7ad65456c45203ffd757050ed1ac38df93e45
Jeremy Cole <jrc436@psu.edu>
2018-02-27 16:28:58 -0500

    refactored into multiple projects with spring version separate

---
+++ spring/src/main/java/ist411/spring/App.java
@@ -0,0 +1,13 @@
+package ist411.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
